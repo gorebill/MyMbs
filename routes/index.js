d9f2bc912e93fc65968a683462a5c03dee07e6ef
gorebill <gorebill@163.com>
2015-02-13 00:32:43 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    //res.render('index', { title: 'Express' });
+    res.redirect('angular')
+});
+
+/* 参考 http://expressjs.com/api.html */
+
+router.get('/angular', function (req, res, next) {
+    res.render('angular');
+});
+
+router.get('/user/:id([0-9]+)', function (req, res) {
+    res.send('user ' + req.params.id);
+});
+
+module.exports = router;
